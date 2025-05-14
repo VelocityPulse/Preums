fbd14337ecbca5f102e686898d5a4ef8c102e5f9
VelocityPulse <velocitypulse@outlook.fr>
2025-05-14 21:15:46 +0200

    update compile version and koin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.velocitypulse.preums"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.velocitypulse.preums"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

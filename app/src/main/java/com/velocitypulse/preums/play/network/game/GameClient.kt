cd4fdd9398daeea687c5424819a1b6573c49e671
VelocityPulse <velocitypulse@outlook.fr>
2025-12-08 22:00:57 +0100

    wip

--- app/src/main/java/com/velocitypulse/preums/play/network/game/GameClient.kt
+++ app/src/main/java/com/velocitypulse/preums/play/network/game/GameClient.kt
@@ -6,7 +6,11 @@ import com.velocitypulse.preums.play.network.ClientInfo
 import com.velocitypulse.preums.play.network.ServerInfo
 import com.velocitypulse.preums.play.network.core.NetHelper
 import com.velocitypulse.preums.play.network.discovery.getLocalIP
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.encodeToString
@@ -16,6 +20,22 @@ private const val TAG = "GameClient"
 
 class GameClient(private val serverInfo: ServerInfo) {
 
+    private var socket: Socket? = null
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
+    private val _events = MutableSharedFlow<GameEvent>()
+    val events: SharedFlow<GameEvent> = _events.asSharedFlow()
+
+    suspend fun connect() {
+        // TODO
+    }
+
+    suspend fun send(message: String) {
+        // TODO
+    }
+
+    suspend fun disconnect() {
+        // TODO
+    }
 
 }
